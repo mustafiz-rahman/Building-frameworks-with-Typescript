6c266564aa709b63c3edefa06949a1219451ab60
mustafiz-rahman <mustafiz.iub@gmail.com>
2021-11-02 01:18:04 +0600

    implimenting inheritance

---
+++ src/models/ApiSync.ts
@@ -0,0 +1,22 @@
+import axios, { AxiosPromise, AxiosResponse } from "axios";
+
+interface hasId{
+    id?:number;
+}
+
+export class ApiSync<T extends hasId> {
+  constructor(public rootUrl: string) {}
+  fetch(id: number): AxiosPromise {
+    return axios.get(`${this.rootUrl}/${id}`);
+  }
+  save(data: T): AxiosPromise {
+    const { id } = data;
+    if (id) {
+      //put
+      return axios.put(`${this.rootUrl}/${id}`, data);
+    } else {
+      //post
+      return axios.post(this.rootUrl, data);
+    }
+  }
+}
---
+++ src/models/Model.ts
@@ -0,0 +1,68 @@
+import { AxiosPromise , AxiosResponse} from "axios";
+
+interface ModelAttribute<T>{
+    set(value:T):void;
+    getAll():T;
+    get<K extends keyof T>(key:K):T[K];
+
+}
+interface Sync<T>{
+    fetch(id:number):AxiosPromise;
+    save(data:T):AxiosPromise;
+
+}
+
+interface Events{
+    on(eventName:string,callBack:()=>void):void;
+    trigger(eventName:string):void;
+}
+interface HasId{
+    id?:number;
+}
+
+export class Model<T extends HasId>{
+    constructor(
+        private attribute:ModelAttribute<T>,
+        private events:Events,
+        private sync:Sync<T>
+    ){}
+
+    get on() {
+        return this.events.on;
+      }
+      get trigger() {
+        return this.events.trigger;
+      }
+      get get() {
+        return this.attribute.get;
+      }
+      set(update:T):void{
+        this.attribute.set(update);
+        this.events.trigger('change');
+      }
+      fetch():void{
+        
+        const id= this.attribute.get('id');
+    
+        if(typeof id !=='number')
+        {
+          throw new Error('Cant fetch');
+        }
+        this.sync.fetch(id).then((res:AxiosResponse):void=>{
+          this.set(res.data);
+        })
+      } 
+    
+      save():void{
+        this.sync.save(this.attribute.getAll())
+        .then((res:AxiosResponse):void=>{
+          this.trigger('Save')
+        })
+        .catch(()=>{
+          this.trigger('error');
+        });
+        
+      }
+
+    
+}
